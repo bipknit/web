f9507a8ada9ca4d13c7005ed0e9ff346d1310c39
bipknit <brinyipeti@mailbox.unideb.hu>
2025-07-22 21:34:44 +0200

    third commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,72 +20,72 @@ const Navbar = () => {
   }, [toggle]);
 
   const renderNavLinks = (isSecondary) => (
-    <ul className={`list-none ${isSecondary ? 'flex sm:hidden' : 'hidden sm:flex'} flex-row gap-6`}>
-      {navLinks.map((link) => (
+      <ul className={`list-none ${isSecondary ? 'flex sm:hidden' : 'hidden sm:flex'} flex-row gap-6`}>
+        {navLinks.map((link) => (
+            <li
+                key={link.id}
+                className={`${
+                    active === link.title ? 'text-white' : isSecondary ? 'text-secondary' : 'text-white'
+                } hover:text-white text-[20px] font-medium cursor-pointer`}
+                onClick={() => {
+                  setActive(link.title);
+                  if (isSecondary) {
+                    setToggle(false);
+                  }
+                }}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+        ))}
         <li
-          key={link.id}
-          className={`${
-            active === link.title ? 'text-white' : isSecondary ? 'text-secondary' : 'text-white'
-          } hover:text-white text-[20px] font-medium cursor-pointer`}
-          onClick={() => {
-            setActive(link.title);
-            if (isSecondary) {
-              setToggle(false);
-            }
-          }}
+            className={`text-${
+                isSecondary ? 'secondary' : 'white'
+            } hover:text-white text-[20px] font-medium cursor-pointer`}
         >
-          <a href={`#${link.id}`}>{link.title}</a>
+          <button onClick={toggleResume}>Resume</button>
         </li>
-      ))}
-      <li
-        className={`text-${
-          isSecondary ? 'secondary' : 'white'
-        } hover:text-white text-[20px] font-medium cursor-pointer`}
-      >
-        <button onClick={toggleResume}>Resume</button>
-      </li>
-    </ul>
+      </ul>
   );
 
   return (
-    <>
-      <nav
-        className={`${styles.paddingX} w-full flex items-center py-3 fixed top-0 z-20 bg-primary`}
-      >
-        <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-          <Link
-            to="/"
-            className="flex items-center gap-2"
-            onClick={() => {
-              setActive('');
-              window.scrollTo(0, 0);
-            }}
-          >
-            <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-            <p className="text-white text-[20px] font-bold cursor-pointer flex">
-              LOHIT&nbsp;
-              <span className="sm:block hidden">KOLLURI</span>
-            </p>
-          </Link>
-          {renderNavLinks(false)}
-          <div className="sm:hidden flex flex-1 justify-end items-center">
-            <img
-              src={toggle ? close : menu}
-              alt="menu"
-              className="w-[28px] h-[18px] object-contain cursor-pointer"
-              onClick={() => setToggle(!toggle)}
-            />
-            <div
-              className={`p-4 black-gradient absolute top-14 right-0 mx-2 my-2 min-w-[120px] z-10 rounded-xl foggy-glass ${
-                toggle ? 'flex' : 'hidden'
-              }`}
+      <>
+        <nav
+            className={`${styles.paddingX} w-full flex items-center py-3 fixed top-0 z-20 bg-primary`}
+        >
+          <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+            <Link
+                to="/"
+                className="flex items-center gap-2"
+                onClick={() => {
+                  setActive('');
+                  window.scrollTo(0, 0);
+                }}
             >
-              {renderNavLinks(true)}
+              <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+              <p className="text-white text-[20px] font-bold cursor-pointer flex">
+                bipknit&nbsp;
+                <span className="sm:block hidden"></span>
+              </p>
+            </Link>
+            {renderNavLinks(false)}
+            <div className="sm:hidden flex flex-1 justify-end items-center">
+              <img
+                  src={toggle ? close : menu}
+                  alt="menu"
+                  className="w-[28px] h-[18px] object-contain cursor-pointer"
+                  onClick={() => setToggle(!toggle)}
+              />
+              <div
+                  className={`p-4 black-gradient absolute top-14 right-0 mx-2 my-2 min-w-[120px] z-10 rounded-xl foggy-glass ${
+                      toggle ? 'flex' : 'hidden'
+                  }`}
+              >
+                {renderNavLinks(true)}
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
-    </>
+        </nav>
+      </>
   );
 };
 
